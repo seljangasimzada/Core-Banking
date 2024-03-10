6bab0e1b6596319cc456c1b79f24a230e4f6e6c2
seljangasimzada <selcan.gasimzada@gmail.com>
2024-03-10 13:17:54 +0400

    Update Customers.sql

--- Customers.sql
+++ Customers.sql
@@ -1,34 +1,57 @@
-crate TABLE cb_customers(
-    id number generated always as identity primary key,
-    first_name varchar2(30),
-    last_name varchar2(30),
-    birt_date date,
-    gender char(1));
-    
-create or replace procedure cb_customers_insert(
-p_first_name varchar2,
-p_last_name varchar2,
-p_birth_date date,
-p_gender char) is 
-begin insert into cb_customers(first_name, last_name, birth_date, gender)
-values(p_first_name, p_last_name, p_birth_date, p_gender );
-commit;
-end;
+DROP TABLE cb_customers;
 
-create or replace procedure cb_customers_update(
-p_id number,
-p_first_name varchar2 default null,
-p_last_name varchar2 default null,
-p_birth_date date default null,
-p_gender char default null) 
-is
-begin 
-update cb_customers set 
-first_name=nvl(p_first_name, first_name),
-last_name = nvl(p_last_name, last_name),
-birth_date = nvl(p_birth_date, birth_date),
-gender = nvl(p_gender, gender)
-WHERE
-    id = p_id;
-commit;
-end;
+CREATE TABLE cb_customers (
+    id         NUMBER
+        GENERATED ALWAYS AS IDENTITY
+    PRIMARY KEY,
+    first_name VARCHAR2(30),
+    last_name  VARCHAR2(30),
+    birth_date DATE,
+    gender     CHAR(1)
+);
+
+CREATE OR REPLACE PROCEDURE cb_customers_insert (
+    p_first_name VARCHAR2,
+    p_last_name  VARCHAR2,
+    p_birth_date DATE,
+    p_gender     CHAR
+) IS
+BEGIN
+    INSERT INTO cb_customers (
+        first_name,
+        last_name,
+        birth_date,
+        gender
+    ) VALUES (
+        p_first_name,
+        p_last_name,
+        p_birth_date,
+        p_gender
+    );
+
+    COMMIT;
+END;
+
+CREATE OR REPLACE PROCEDURE cb_customers_update (
+    p_id         NUMBER,
+    p_first_name VARCHAR2 DEFAULT NULL,
+    p_last_name  VARCHAR2 DEFAULT NULL,
+    p_birth_date DATE DEFAULT NULL,
+    p_gender     CHAR DEFAULT NULL,
+    p_result     OUT VARCHAR2
+) IS
+BEGIN
+    UPDATE cb_customers
+    SET
+        first_name = nvl(p_first_name, first_name),
+        last_name = nvl(p_last_name, last_name),
+        birth_date = nvl(p_birth_date, birth_date),
+        gender = nvl(p_gender, gender)
+    WHERE
+        id = p_id;
+
+    IF SQL%rowcount = 0 THEN
+        p_result := 'Not found  any data by id ' || p_id;
+    END IF;
+    COMMIT;
+END;
